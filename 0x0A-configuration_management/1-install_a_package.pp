67a9f51e8ef60e11b0c6facc4190b86717c61c2d
Alemi Herbert <alemiherbert@gmail.com>
2024-01-22 07:33:02 +0300

    Conform all files to puppet-lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,6 +3,6 @@
 # Install a specific version of flask
 
 package {'flask':
-    ensure  => '2.1.0',
+    ensure   => '2.1.0',
     provider => 'pip3'
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Execute a command
 
 exec {'pkill':
-    command => 'pkill killmenow',
+    command  => 'pkill killmenow',
     provider => 'shell'
 }
