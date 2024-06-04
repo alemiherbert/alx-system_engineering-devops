1318fd54285c13ed3520f2b6dc337bb9e41e6872
Alemi Herbert <alemiherbert@gmail.com>
2024-06-04 11:34:30 +0300

    Pycodestyle compliance

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Returns to-do list information for a given employee ID."""
+
 import requests
 import sys
 
@@ -12,4 +13,3 @@ if __name__ == "__main__":
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(c)) for c in completed]
-
